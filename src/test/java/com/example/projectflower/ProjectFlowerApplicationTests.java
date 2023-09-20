2004748c8efdd75028247b14304b9c2f8faa564e
donggyun <yje07052@naver.com>
2023-09-20 18:12:03 +0900

    1

---
+++ src/test/java/com/example/projectflower/ProjectFlowerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectflower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectFlowerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
