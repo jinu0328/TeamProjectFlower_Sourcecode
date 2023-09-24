c9eaa1d08427e5f23cf2b5ae12ff2d22cfbd63c2
donggyun <yje07052@naver.com>
2023-09-24 14:48:10 +0900

    1st

---
+++ src/main/java/com/FlowerProject/flower/FlowerApplication.java
@@ -0,0 +1,13 @@
+package com.FlowerProject.flower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+public class FlowerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowerApplication.class, args);
+	}
+
+}
