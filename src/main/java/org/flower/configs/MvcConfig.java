d2728d24ec20dfd93d7329cc1dcc811d96db022d
kookong2 <hy010309@naver.com>
2023-10-02 22:23:48 +0900

    UI 틀 추가
    관리자 페이지, 사용자 페이지 thymeleaf 파일 추가 완료

--- src/main/java/org/flower/configs/MvcConfig.java
+++ src/main/java/org/flower/configs/MvcConfig.java
@@ -3,13 +3,22 @@ package org.flower.configs;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableJpaAuditing
-@EnableWebMvc
 public class MvcConfig implements WebMvcConfigurer {
+
+    /**
+     * 정적 경로 설정
+     *
+     */
+    public void addResourceHandlers(ResourceHandlerRegistry registry){
+
+        // 파일 업로드 경로 설정
+    }
 
     /**
     *  <메인 페이지> 컨트롤러
