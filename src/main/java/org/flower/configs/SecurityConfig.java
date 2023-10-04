94bd9ccf5d099d1d07ddbb5c7de3afb44d04e3f1
kookong2 <hy010309@naver.com>
2023-10-04 15:47:05 +0900

    Security 적용 일부 완료
    Security 적용했을 때 view가 안되는 오류 수정

--- src/main/java/org/flower/configs/SecurityConfig.java
+++ src/main/java/org/flower/configs/SecurityConfig.java
@@ -2,21 +2,22 @@ package org.flower.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
-    /* 이부분도 좀 더 봐야 할 것 같음,,
-    @Override
-    public void configure(WebSecurity web) {
-        web
-                .ignoring() // spring security 필터 타지 않도록 설정
-                .antMatchers("/resources/**") // 정적 리소스 무시
-                .antMatchers("/h2-console/**"); // h2-console 무시
-    }
-    */
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeRequests()
+                .anyRequest().permitAll();
 
-    /*
+        return http.build();
+    }
+
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         return w -> w.ignoring() // 시큐리티가 무시할 정적 경로 설정
@@ -33,5 +34,4 @@ public class SecurityConfig {
                         "/uploads/**");
     }
 
-     */
 }
