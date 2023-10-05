bc19d966d6cc7e3fabc83c10eac5ded2b77361bd
kookong2 <hy010309@naver.com>
2023-10-05 20:18:47 +0900

    join 기능 구현중
    UserJoin 기능 구현 중

--- src/main/java/org/flower/controllers/user/user/UserJoin.java
+++ src/main/java/org/flower/controllers/user/user/UserJoin.java
@@ -1,4 +1,52 @@
 package org.flower.controllers.user.user;
 
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import org.flower.entities.User;
+import org.modelmapper.ModelMapper;
+
+/*
+* 회원 가입 커맨드 객체 생성 클래스
+* */
+@Data
 public class UserJoin {
+
+    @NotBlank @Email
+    private String userEmail;   // 이메일
+
+    @NotBlank
+    private String userPw;      // 비밀번호
+
+    @NotBlank
+    private String userPwCk;    // 비밀번호 확인
+
+    @NotBlank
+    private String userNm;      // 회원명
+
+    private String birth;       // 생년월일
+
+    private String cellPhone;   // 휴대전화번호
+
+    private String address;     // 주소
+
+    @AssertTrue
+    private boolean termsAgree; // 회원가입 약관동의
+
+    // User 엔티티로 변환
+    public static User of(UserJoin userJoin){
+
+        return new ModelMapper().map(userJoin, User.class);
+    }
+
+    private String authCode;
+
+    public String getAuthCode(){
+        return authCode;
+    }
+
+    public void setAuthCode(String authCode){
+        this.authCode = authCode;
+    }
 }
--- src/main/java/org/flower/models/user/UserJoinService.java
+++ src/main/java/org/flower/models/user/UserJoinService.java
@@ -1,4 +1,35 @@
 package org.flower.models.user;
 
+import lombok.RequiredArgsConstructor;
+import org.flower.commons.constants.UserRole;
+import org.flower.controllers.user.user.UserJoin;
+import org.flower.entities.User;
+import org.flower.repositories.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UserJoinService {
+
+    private final UserRepository repository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    public void join(UserJoin join){
+
+        String hash = passwordEncoder.encode(join.getUserPw());
+
+        User user = User.builder()
+                .userEmail(join.getUserEmail())
+                .userPw(hash)
+                .userNm(join.getUserNm())
+                .cellPhone(join.getCellPhone())
+                .role(UserRole.USER)    // 기본
+                .birth(join.getBirth())
+                .address(join.getAddress())
+                .build();
+
+        repository.save(user);
+    }
 }
