990e6a1942e7367896d8a07b7963a28f84e04d22
d0kyoung <ehrud0522@gmail.com>
2023-10-05 14:45:56 +0900

    flowerinfo, floweriamge 엔티티 추가

---
+++ src/main/java/org/flower/entities/FlowerImage.java
@@ -0,0 +1,21 @@
+package org.flower.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+@Table(name = "flowerimage")
+
+public class FlowerImage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "image_id")
+    private Long imageId;
+
+    @ManyToOne
+    @JoinColumn(name = "flowerNo")
+    private flowerinfo flower;
+}
