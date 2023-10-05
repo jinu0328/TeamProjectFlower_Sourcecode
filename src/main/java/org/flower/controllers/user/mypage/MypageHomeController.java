a8308c0794a60c22ee6b9ee3f24ca0823032782d
kookong2 <hy010309@naver.com>
2023-10-05 14:05:27 +0900

    마이페이지 경로 수정 완료
    마이페이지 경로 수정 완료

--- src/main/java/org/flower/controllers/user/mypage/MypageHomeController.java
+++ src/main/java/org/flower/controllers/user/mypage/MypageHomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/mypage")
+@RequestMapping("/user/mypage")
 public class MypageHomeController {
     @GetMapping
     public String main(){
