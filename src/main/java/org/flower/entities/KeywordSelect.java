b0f017d165baff9bf83465e5a60dc3a192a9a6b7
d0kyoung <ehrud0522@gmail.com>
2023-10-09 00:36:54 +0900

    keywordselect, flowerkeywordmapping 엔티티 추가

---
+++ src/main/java/org/flower/entities/KeywordSelect.java
@@ -0,0 +1,30 @@
+package org.flower.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@ToString(exclude = {"user", "keyword"})
+@EqualsAndHashCode(exclude = {"user","keyword"})
+@Table(name="keywordselect")  // 'keywordselect' 속성으로 해당 엔티티가 매핑될 테이블의 이름을 지정
+public class KeywordSelect {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long SelectionId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userNo")
+    private User user;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "keywordNo")
+    private Keywords keyword;
+
+    @Column(name = "weight", columnDefinition = "INT DEFAULT 0")
+    private int weight;
+
+}
--- src/main/java/org/flower/entities/Keywords.java
+++ src/main/java/org/flower/entities/Keywords.java
@@ -12,8 +12,8 @@ import org.flower.commons.constants.UserRole;
 @NoArgsConstructor      // 파라미터가 없는 기본 생성자를 생성
 @AllArgsConstructor     // 모든 필드를 파라미터로 갖는 생성자를 생성
 @Entity                 // 이 클래스가 JPA 엔티티임을 나타낸다. 즉 이 클래스의 인스턴스들은 데이터베이스의 레코드와 매핑된다.
-@Table(name="keywords")    // 'name' 속성으로 해당 엔티티가 매핑될 테이블의 이름을 지정함
-public class Keywords extends BaseEntity{
+@Table(name="keywords")    // 'keywords' 속성으로 해당 엔티티가 매핑될 테이블의 이름을 지정
+public class Keywords {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
