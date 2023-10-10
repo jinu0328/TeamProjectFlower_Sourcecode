1f19ca0d574fc9ac0e40732ebc6794b11f0e1dba
kookong2 <hy010309@naver.com>
2023-10-11 02:07:29 +0900

    로그인, 회원가입 에러 메세지 완성
    로그인, 회원가입 폼에 잘못된 데이터 혹은 빈 값을 넣었을 때 에러메세지 뜨는 기능 완성

--- src/main/java/org/flower/models/user/LoginFailureHandler.java
+++ src/main/java/org/flower/models/user/LoginFailureHandler.java
@@ -4,41 +4,45 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-import jakarta.validation.constraints.NotBlank;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.web.servlet.FlashMap;
+import org.springframework.web.servlet.FlashMapManager;
+import org.springframework.web.servlet.support.SessionFlashMapManager;
 
 import java.io.IOException;
 import java.util.ResourceBundle;
 
-
 public class LoginFailureHandler implements AuthenticationFailureHandler {
 
     private static ResourceBundle bundle = ResourceBundle.getBundle("messages.validation");
+
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
         HttpSession session = request.getSession();
-        session.removeAttribute("requiredUserEmail");
-        session.removeAttribute("requiredUserPw");
-        session.removeAttribute("loginFail");
-        session.removeAttribute("userEmail");
+
+        // spring 에서 제공하는 Flash Attributes를 사용하여 에러메세지를 일회성으로 전달하도록 함
+        FlashMap flashMap = new FlashMap();
 
         String userEmail = request.getParameter("userEmail");
         String userPw = request.getParameter("userPw");
 
-        session.setAttribute("userEmail", userEmail);
+        flashMap.put("userEmail", userEmail);
 
         if(userEmail == null || userEmail.isBlank()){
-            session.setAttribute("requiredUserEmail", bundle.getString("NotBlank.emil"));
+            flashMap.put("requiredUserEmail", bundle.getString("NotBlank.email"));
         }
 
         if(userPw == null || userPw.isBlank()){
-            session.setAttribute("requiredUserPw", bundle.getString("NotBlank.userPw"));
+            flashMap.put("requiredUserPw", bundle.getString("NotBlank.userPw"));
         }
 
-        if(userEmail != null && !userEmail.isBlank() && userPw !=null && !userPw.isBlank()){
-            session.setAttribute("loginFail", bundle.getString("user.login.fail"));
+        if(userEmail != null && !userEmail.isBlank() && userPw != null && !userPw.isBlank()){
+            flashMap.put("loginFail", bundle.getString("user.login.fail"));
         }
+
+        FlashMapManager flashMapManager = new SessionFlashMapManager();
+        flashMapManager.saveOutputFlashMap(flashMap, request, response);
 
         String url = request.getContextPath() + "/user/login";
         response.sendRedirect(url);
--- src/main/resources/static/front/js/common.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('login-btn').addEventListener('click', function() {
-    document.getElementById('login-form').style.display = 'block';
-});
-
-document.getElementById('signup-btn').addEventListener('click', function() {
-    document.getElementById('signup-form').style.display = 'block';
-});
