bf06e59c5a56faafb8e112a61a0ceca408e1d50e
kookong2 <hy010309@naver.com>
2023-10-12 19:20:59 +0900

    .
    .

--- src/main/java/org/flower/controllers/admin/user/UserController.java
+++ src/main/java/org/flower/controllers/admin/user/UserController.java
@@ -1,8 +1,16 @@
 package org.flower.controllers.admin.user;
 
+import lombok.Data;
+import org.flower.entities.User;
+import org.flower.models.user.UserInfo;
+import org.flower.models.user.UserInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/admin/user")
@@ -15,7 +23,7 @@ public class UserController {
     *
     * */
     @GetMapping
-    public String index(){
+    public String index(Model model){
 
         return "admin/user/index";
     }
