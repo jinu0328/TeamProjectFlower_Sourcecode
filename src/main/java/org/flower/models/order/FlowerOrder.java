ac3f27dc97602c767d90906b52e47d271f912817
d0kyoung <ehrud0522@gmail.com>
2023-10-15 01:01:27 +0900

    주문관리 기능

--- src/main/java/org/flower/models/order/FlowerOrder.java
+++ src/main/java/org/flower/models/order/FlowerOrder.java
@@ -1,4 +1,19 @@
 package org.flower.models.order;
 
+import org.flower.entities.Order;
+import org.flower.repositories.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class FlowerOrder {
+
+    @Autowired
+    private OrderRepository repository;
+    public List<Order> getAllOrders(){
+        return repository.findAll();
+    }
+
 }
