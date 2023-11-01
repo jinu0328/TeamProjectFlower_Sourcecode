3c2dcbb2bea277c8eae048722176031a71fbb947
donggyun <yje07052@naver.com>
2023-11-01 09:56:05 +0900

    UserEdit, UserEditValidator, UserEditInfo, UserEditService 추가
    UserEdit, UserEditValidator, UserEditInfo, UserEditService 추가

---
+++ src/main/java/org/flower/models/user/UserEditInfo.java
@@ -0,0 +1,15 @@
+package org.flower.models.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserEditInfo {
+    private Long userNo;        // User Number
+    private String userNickNm;  // Nickname
+}
