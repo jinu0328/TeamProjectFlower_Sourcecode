cc4d082e297bb7e3a5d6944d408df70e457facdf
kookong2 <hy010309@naver.com>
2023-11-01 19:11:45 +0900

    꽃추천 로딩 페이지 구현중
    아직 구현중

--- src/main/resources/static/front/recommend/js/loading.js
+++ src/main/resources/static/front/recommend/js/loading.js
@@ -1,7 +1,7 @@
 // 로딩 페이지에서 7초후 자동으로 결과 페이지로 이동
-document.addEventListener('DOMContentLoaded', function() {
+/*document.addEventListener('DOMContentLoaded', function() {
     setTimeout(function() {
         const keywordNos = document.querySelector('body').getAttribute('data-keyword-nos');
         window.location.href = `http://localhost:3001/recommend/result?keywordNos=${keywordNos}`;
     }, 7000);  // 7초 후 실행
-});
+});*/
