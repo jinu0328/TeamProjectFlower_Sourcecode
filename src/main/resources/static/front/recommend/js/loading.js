0cc13f86b30f8b094c73bcc39c51bebcfcdaa9fa
kookong2 <hy010309@naver.com>
2023-11-02 00:09:25 +0900

    로딩 페이지 구현중
    구현중

--- src/main/resources/static/front/recommend/js/loading.js
+++ src/main/resources/static/front/recommend/js/loading.js
@@ -3,5 +3,30 @@
     setTimeout(function() {
         const keywordNos = document.querySelector('body').getAttribute('data-keyword-nos');
         window.location.href = `http://localhost:3001/recommend/result?keywordNos=${keywordNos}`;
-    }, 7000);  // 7초 후 실행
+    }, 5000);  // 5초 후 실행
 });*/
+
+document.addEventListener("DOMContentLoaded", function() {
+    const iconContainer = document.getElementById('iconContainer');
+    let count = 0;
+
+    const addIcon = () => {
+        const icon = document.createElement('i');
+        icon.className = 'xi-garden';
+        iconContainer.appendChild(icon);
+
+        setTimeout(() => {
+            icon.classList.add('visible');
+        }, 50);
+
+        count++;
+
+        if (count < 5) {
+            setTimeout(addIcon, 1000);
+        }
+    };
+
+    addIcon();
+});
+
+
