36a1ce961353d083eab6ad9f9a6e633367f06a61
d0kyoung <ehrud0522@gmail.com>
2023-11-04 20:02:05 +0900

    주문서 작성 UI 구현 완료!
    주문관리 페이지에 작성한 주문서 양식이 들어가는 것 확인.
    소형꽃다발, 중형꽃다발, 대형꽃다발 사진을 추가해서 사용자가 꽃다발 사이즈를 선택할때 도움을 줄 수 있음
    꽃 추천 결과페이지에 헤더 이미지를 추가하고 RESULT로 뜨게함.

--- src/main/java/org/flower/models/order/orderform/OrderFormService.java
+++ src/main/java/org/flower/models/order/orderform/OrderFormService.java
@@ -5,9 +5,12 @@ import jakarta.transaction.Transactional;
 import org.flower.commons.constants.OrderState;
 import org.flower.entities.Order;
 import org.flower.entities.User;
+import org.flower.models.user.UserInfo;
 import org.flower.repositories.OrderRepository;
 import org.flower.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -22,9 +25,11 @@ public class OrderFormService {
     @Transactional
     public Order submitOrderForm(OrderFormInfo orderFormInfo) {
         // 주문 폼 정보를 바탕으로 새로운 주문 객체를 생성
-        User user = userRepository.findByUserNo(orderFormInfo.getUserNo());
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserInfo currentUser = (UserInfo) authentication.getPrincipal();
+        User user = userRepository.findByUserNo(currentUser.getUserNo());
         if(user == null) {
-            throw new IllegalArgumentException("No user found with userNo " + orderFormInfo.getUserNo());
+            throw new IllegalArgumentException("No user found with userNo " + currentUser.getUserNo());
         }
 
         Order newOrder = Order.builder()
