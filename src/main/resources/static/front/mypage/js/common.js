5eb0504f80761967630351a1f4308ac8f4234f45
donggyun <yje07052@naver.com>
2023-11-04 18:55:19 +0900

    닉네임 수정 기능 구현
    닉네임 수정 기능 추가 완료
    반영하려면 다시 로그인해야함
    추후에 세션 업데이트 코드 추가하여 닉네임 수정시 바로 적용되게 할 예정

--- src/main/resources/static/front/mypage/js/common.js
+++ src/main/resources/static/front/mypage/js/common.js
@@ -18,6 +18,17 @@ function getUserNo() {
 
 
 $(document).ready(function() {
+
+    var csrfToken = $("meta[name='_csrf']").attr("content");
+    var csrfHeader = $("meta[name='_csrf_header']").attr("content");
+
+    // 모든 AJAX 요청에 CSRF 토큰을 헤더로 보내도록 설정
+    $.ajaxSetup({
+        beforeSend: function(xhr) {
+            xhr.setRequestHeader(csrfHeader, csrfToken);
+        }
+    });
+
     const nicknameEditButton = $('#nicknameEditButton');
     const nicknameEditBox = $('#nicknameEditBox');
     const confirmButton = $('#confirmButton');
@@ -34,12 +45,6 @@ $(document).ready(function() {
     });
 
 
-    confirmButton.on('click', function() {
-        // Reset everything to original state
-        nicknameEditBox.hide();
-        confirmButton.hide();
-        nicknameEditButton.show();
-    });
 
 
     checkDuplicateButton.on('click', function() {
@@ -53,8 +58,6 @@ $(document).ready(function() {
         // 여기서 $.ajax를 사용하여 닉네임 중복 확인 로직을 수행합니다.
         $.ajax({
             type: "GET",
-
-            url: "/user/mypage/profile/profilePage/checkNickNm", // 요청 URL
 
             url: "/user/mypage/profile/check/checkNickNm", // 요청 URL
             data: { newNickname: newNickname },  // 전송할 데이터
@@ -75,34 +78,7 @@ $(document).ready(function() {
 
 
 
-    confirmButton.addEventListener('click', function() {
-        const newNickname = nicknameInput.value;
 
-        // 서버에 닉네임 변경 요청을 전송합니다:
-        // (이 부분도 실제 프로젝트에 맞게 수정해야 합니다.)
-        fetch('/updateNickname', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                // 필요한 다른 헤더들
-            },
-            body: JSON.stringify({ newNickname: newNickname })
-        })
-            .then(response => response.json())
-            .then(data => {
-                if (data.isSuccess) {
-                    alert('닉네임이 변경되었습니다.');
-                    document.querySelector('.name').innerText = newNickname;
-                    nicknameEditBox.style.display = 'none';
-                } else {
-                    alert('닉네임 변경에 실패했습니다.');
-                }
-            })
-            .catch(error => {
-                console.error("Error during fetch:", error);
-                alert('닉네임 변경 중 오류가 발생했습니다.');
-            });
-    });
 
 
    confirmButton.on('click', function() {
@@ -124,16 +100,15 @@ $(document).ready(function() {
             data: JSON.stringify(userEditInfo),
             dataType: "json",
             success: function(response) {
-                if (response.message) {
-                    // 성공 메시지를 표시하거나 다른 UI 업데이트를 수행
-                    console.log(response.message);
-                    // 예를 들어, 페이지 내 닉네임을 표시하는 요소를 업데이트 할 수 있습니다.
-                    nicknameEditBox.hide();
-                    confirmButton.hide();
-                    nicknameEditButton.show();
-                }
-            },
-            error: function(xhr, status, error) {
+                // 서버에서 응답으로 온 UserEditInfo 객체 사용
+                alert('닉네임이 변경되었습니다. 다시 로그인 해주세요');
+                console.log('Nickname updated to: ', response.userNickNm);
+                // 페이지 내 닉네임을 표시하는 요소를 업데이트 할 수 있습니다.
+                $('#someElementToShowNickname').text(response.userNickNm);
+                nicknameEditBox.hide();
+                confirmButton.hide();
+                nicknameEditButton.show();
+            },or: function(xhr, status, error) {
                 // 오류 메시지를 표시
                 alert("An error occurred: " + xhr.responseText);
             }
