5985002c96cb3d63f764cb337f576d01de4e4dc7
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2023-11-16 23:28:52 +0900

    엽서페이지 꽃 이름 한글로 변경

--- src/main/resources/static/front/postcard/js/common.js
+++ src/main/resources/static/front/postcard/js/common.js
@@ -1,14 +1,17 @@
-function allowDrop(ev) {
+function allowDropFlower(ev) {
     ev.preventDefault();
 }
 
-    function drag(ev) {
-    ev.dataTransfer.setData("text", ev.target.innerText);
+function dragFlower(ev) {
+    // data-englishnm 속성에서 영문 이름을 가져옵니다.
+    var englishNm = ev.target.getAttribute('data-englishnm');
+    ev.dataTransfer.setData("text", englishNm);
 }
 
-    function drop(ev) {
+function dropFlower(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
     var promptInput = document.getElementById("prompt");
     promptInput.value += data + " "; // 여기서 공백을 추가하여 여러 이름을 연결
 }
+
