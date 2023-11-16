a8a3e5b761d967505b5ac3d3d67482132bc353fc
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2023-11-16 14:07:12 +0900

    엽서 기능 수정

--- src/main/java/org/flower/controllers/postCard/OpenAIController.java
+++ src/main/java/org/flower/controllers/postCard/OpenAIController.java
@@ -1,7 +1,9 @@
 package org.flower.controllers.postCard;
 
+import org.flower.entities.Flower;
 import org.flower.models.postCard.OpenAIService;
 import org.flower.models.postCard.OpenAiInfo;
+import org.flower.models.recommend.flower.FlowerInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +18,9 @@ import java.util.Map;
 @RequestMapping("/postCard")
 public class OpenAIController {
 
+    @Autowired
+    FlowerInfoService flowerInfoService;
+
     private final OpenAIService openAIService;
 
     @Autowired
@@ -25,23 +30,24 @@ public class OpenAIController {
 
     @GetMapping("/create-image")
     public String showCreateImageForm(Model model) {
+        List<Flower> flowers = flowerInfoService.getAllFlowers();
         model.addAttribute("request", new OpenAiInfo()); // ImageRequest는 prompt 필드를 갖는 폼 오브젝트입니다.
+        model.addAttribute("flowers", flowers);
         return "front/postcard/main"; // create-image.html 뷰로 맵핑됩니다.
     }
 
     @PostMapping("/create-image")
     public String createImage(@RequestParam("prompt") String prompt, Model model) {
+        prompt = "a postcard with" + prompt + "on it";
         ResponseEntity<Map<String, Object>> response = openAIService.createImage(prompt);
         List<Map<String, Object>> dataList = (List<Map<String, Object>>) response.getBody().get("data");
         String imageUrl = "";
         if (dataList != null && !dataList.isEmpty()) {
-            // 첫 번째 아이템의 맵에서 'url' 키에 해당하는 값을 가져옵니다.
+            // 첫 번째 아이템의 맵에서 'url' 키에 해당하는 값을 가져옴.
             imageUrl = (String) dataList.get(0).get("url");
-            // imageUrl 변수를 사용합니다. 예를 들어 콘솔에 출력하거나 모델에 추가할 수 있습니다.
-            System.out.println(imageUrl);
+            // imageUrl 변수를 사용. 예를 들어 콘솔에 출력하거나 모델에 추가.
         }
-       model.addAttribute("imageData", imageUrl);
-//        System.out.println(response.getBody().get("data"));
+        model.addAttribute("imageData", imageUrl);
         return "front/postcard/result"; // result.html로 반환됩니다.
     }
 
