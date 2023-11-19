c4c59a8e9d8c1c167c0c6291eb75116c569d151a
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2023-11-19 16:35:24 +0900

    엽서 추천 페이지 수정

--- src/main/resources/static/front/postcard/js/common.js
+++ src/main/resources/static/front/postcard/js/common.js
@@ -3,15 +3,33 @@ function allowDropFlower(ev) {
 }
 
 function dragFlower(ev) {
-    // data-englishnm 속성에서 영문 이름을 가져옵니다.
-    var englishNm = ev.target.getAttribute('data-englishnm');
-    ev.dataTransfer.setData("text", englishNm);
+    var dragData = {
+        flowerNm: ev.target.innerText,
+        englishNm: ev.target.getAttribute('data-englishnm')
+    };
+    ev.dataTransfer.setData("text", JSON.stringify(dragData));
 }
 
 function dropFlower(ev) {
     ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
+    var dragData = JSON.parse(ev.dataTransfer.getData("text"));
+    var droppedFlowersTable = document.getElementById("droppedFlowersBody");
+
+    // 기존에 드롭된 꽃이 있으면 모두 제거
+    while (droppedFlowersTable.firstChild) {
+        droppedFlowersTable.removeChild(droppedFlowersTable.firstChild);
+    }
+
+    // 새로 드롭된 꽃 추가
+    var newTableRow = droppedFlowersTable.insertRow();
+    var newCell = newTableRow.insertCell(0);
+    newCell.innerText = dragData.flowerNm;
+
     var promptInput = document.getElementById("prompt");
-    promptInput.value += data + " "; // 여기서 공백을 추가하여 여러 이름을 연결
+    promptInput.value = dragData.englishNm; // 기존에 저장된 꽃의 영문 이름을 새로운 것으로 대체
+
+    var dropZoneImage = document.getElementById("dropZoneImage");
+    dropZoneImage.src = "/front/images/openedEnvelope.png";
 }
 
+// checkIfTableIsEmptyAndRemoveRow 함수는 더 이상 필요하지 않으므로 제거
