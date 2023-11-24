22fd1c7fa126d7cb32c429a8411d39d6b00fdb70
d0kyoung <ehrud0522@gmail.com>
2023-11-24 19:00:06 +0900

    주문서 폼 제출 전 필수 입력 항목 검증 기능 개선
    1. 픽업 날짜와 시간 검증: 사용자가 픽업 날짜와 시간을 선택하지 않았을 경우, 각각 '픽업 날짜를 선택해주세요.' 및 '픽업 시간을 선택해주세요.'라는 경고 메시지를 표시합니다.
    2. 꽃 선택 검증: 사용자가 꽃을 선택하지 않았을 경우, '주문하실 꽃을 선택해주세요.'라는 경고 메시지를 표시합니다. 이를 위해 모든 꽃 선택 input 태그에서 checked 상태를 확인합니다.

--- src/main/resources/static/front/order/js/common.js
+++ src/main/resources/static/front/order/js/common.js
@@ -1,20 +1,19 @@
-
-    document.getElementById('submitOrderForm').addEventListener('click', function() {
+document.getElementById('submitOrderForm').addEventListener('click', function(event) {
     var pickupDate = document.getElementById('pickupDate').value;
-    if (pickupDate === '') {
-    alert('픽업 날짜를 선택해주세요.');
-} else {
-    document.getElementById('orderForm').submit();
-}
-});
-
-
-
-    document.getElementById('submitOrderForm').addEventListener('click', function() {
     var pickupTime = document.getElementById('pickupTime').value;
-    if (pickupTime === '') {
-    alert('픽업 날짜를 선택해주세요.');
-} else {
-    document.getElementById('orderForm').submit();
-}
+    var flowerOptions = document.querySelectorAll('input[name="flowertype"]');
+    var isFlowerSelected = Array.from(flowerOptions).some(option => option.checked);
+
+    if (pickupDate === '') {
+        alert('픽업 날짜를 선택해주세요.');
+        event.preventDefault();
+    } else if (pickupTime === '') {
+        alert('픽업 시간을 선택해주세요.');
+        event.preventDefault();
+    } else if (!isFlowerSelected) {
+        alert('주문하실 꽃을 선택해주세요.');
+        event.preventDefault();
+    } else {
+        document.getElementById('orderForm').submit();
+    }
 });
