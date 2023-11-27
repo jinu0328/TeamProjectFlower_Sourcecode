2650c101e0e8d27d5597c75d08cb3efb22b05da3
donggyun <yje07052@gmail.com>
2023-11-27 23:45:09 +0900

    주문 상태, 마이페이지 UI, css, 엔티티 수

--- src/main/java/org/flower/commons/constants/OrderState.java
+++ src/main/java/org/flower/commons/constants/OrderState.java
@@ -4,5 +4,11 @@ public enum OrderState {
 
     ACCEPTING,   // 접수중
 
-    ACCEPTED // 접수 완료
+    ACCEPTED, // 접수 완료
+
+    PREPARING, // 상품 준비중
+
+    PREPARED, // 준비 완료
+
+    PICKEDUP // 픽업 완료
 }
